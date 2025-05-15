fd2554bb62d017636a6af156d14f3d74bdc00cd7
Iryna R <deutsche.post15@gmail.com>
2025-05-15 17:44:56 +0200

    fix

--- public/js/resizeParticles.js
+++ public/js/resizeParticles.js
@@ -1,10 +1,11 @@
-function resizeParticles() {
-  const vh = window.innerHeight;
-  const particles = document.getElementById("particles-js");
-  if (particles) {
-    particles.style.height = vh + "px";
-  }
-}
-
-window.addEventListener("load", resizeParticles);
-window.addEventListener("resize", resizeParticles);
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    const vh = window.innerHeight;
+    const el = document.getElementById("particles-js");
+    if (el) {
+      el.style.height = vh + "px";
+    } else {
+      console.warn("#particles-js не найден!");
+    }
+  }, 300); // Подождать 300 мс
+});
